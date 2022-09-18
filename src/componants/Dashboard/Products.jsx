4639178d7907a4ce009c228bae60283194f639a7
aliibnemasud <mdmasudrony@gmail.com>
2022-09-18 21:05:52 +0600

    search function working

--- src/componants/Dashboard/Products.jsx
+++ src/componants/Dashboard/Products.jsx
@@ -6,20 +6,35 @@ const Products = () => {
     const [products, setProducts] = useState([]);
     const [sizefilter, setSizeFilter] = useState([]);
     const [pdnameFilter, setPdNamefilter] = useState([]);    
+    const [search, setSearch] = useState([]);    
 
     useEffect(()=> {
         axios.get('products.json')
         .then(res => setProducts(res.data))
     },[])
 
-    const sizeFilter = (e) => {
-        console.log(e.target.value)
+    // Filter by size
+    const sizeFilter = (e) => {        
         const size = e.target.value;
-        const pdSize = products.filter(prodcut => prodcut.size === size)
+        const pdSize = products.filter(product => product.size === size)
         setSizeFilter(pdSize)
+        setPdNamefilter(0)
+    }
+    // Filter By Product Name
+    const filterByName = (e) => {        
+        const pdType = e.target.value;
+        const result = products.filter(product => product.pdtype === pdType)
+        setPdNamefilter(result)
+        setSizeFilter(0)
     }
 
-
+    // Filter by search
+    const searchResult = (e) => {
+        const searchText = e.target.value;
+        const result = products.filter((product) =>
+        product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setSearch(result);        
+    }
 
     let loadProducts;
 
@@ -29,37 +44,36 @@ const Products = () => {
     else if (pdnameFilter.length > 0){
         loadProducts = pdnameFilter
     }
+    else if (search.length > 0){
+        loadProducts = search
+    }
     else {
         loadProducts = products
     }
 
-
-
-
-
-
     return (
         <div>
-            <div className="searchbar flex justify-between">
+            <div className="flex justify-between">
                 <div>
-                    <select className="select mr-2 select-bordered max-w-xs">
-                        <option selected >Hoodies</option>
-                        <option>T-shirt</option>
-                        <option>Pant</option>
+                    <select className="select mr-2 select-bordered max-w-xs" onChange={filterByName}>
+                        <option selected value="hoodies">Hoodies</option>
+                        <option value="t-shirt">T-shirt</option>
+                        <option value="pant">Pant</option>
                     </select>
                     <select className="select select-bordered max-w-xs" onChange={sizeFilter}>
                         <option selected value="m">M</option>
                         <option value="xl">XL</option>
                         <option value="xxl">XXL</option>
                     </select>
+                    <button className='btn btn-ghost ml-2 text-blue-500' onClick={()=> {
+                        setSizeFilter(0)
+                        setPdNamefilter(0)
+                    }}>Reset</button>
                 </div>
                 <div className='flex mb-2'>
                     <div className="form-control mr-2">
                         <div className="input-group">
-                            <input type="text" placeholder="Search…" className="input input-bordered" />
-                            <button className="btn btn-square">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                            </button>
+                            <input onChange={searchResult} type="text" placeholder="Search…" className="input input-bordered" />                           
                         </div>
                     </div>
                     <button className='btn btn-primary'>Add To Cart</button>
